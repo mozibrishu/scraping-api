e892ef9aea1d84618eb65f70cf678f56b010a3da
Mozib <mozib.diu@gmail.com>
2022-09-19 16:26:14 +0600

    scraping basic files ready

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+var allowedOrigins = ['http://localhost:8080',
+    'http://127.0.0.1:5500',
+];
+
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            //return callback(new Error(msg), false);
+             return callback((msg));
+        }
+        return callback(null, true);
+    }
+}));
+
+const home = require('./routes/home');
+const live = require('./routes/live');
+const score = require('./routes/score');
+
+app.use('/', home);
+app.use('/live', live);
+app.use('/score', score);
+
+app.use('/', function(req, res) {
+    res.status(404).json({
+        error: 1,
+        message: 'Data not Found'
+    });
+})
+
+app.listen(port, function() {
+    console.log('listening on port ' + port);
+});
